88023e962e3afd2de3a8ed804b0def1069c241bb
RSN4D <roman@wavefields.at>
2022-04-30 19:19:28 -0700

    Add files via upload
    initial upload

---
+++ ST_Node1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TVN_APP
+{
+    public class ST_Node1
+    {
+        public Thread WorkerThread;
+        private bool Closing;
+        private double Interval;
+
+        // Work Event
+        public EventHandler Work;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="IntervalMilliSeconds"></param>
+        /// <param name="Prio"></param>
+        public ST_Node1(int IntervalMilliSeconds = 1000, ThreadPriority Prio = ThreadPriority.Normal)
+        {
+            Interval = (double)IntervalMilliSeconds;
+
+            WorkerThread = new Thread(new ThreadStart(WorkerLoop));
+            WorkerThread.SetApartmentState(ApartmentState.STA);
+            WorkerThread.Priority = Prio;
+            WorkerThread.IsBackground = true;
+            WorkerThread.Start();
+        }
+
+        /// <summary>
+        /// Sets the interval at runtime
+        /// </summary>
+        public void SetInterval(int IntervalMilliSeconds)
+        {
+            Interval = (double)IntervalMilliSeconds;
+        }
+
+        /// <summary>
+        /// Éndless Thread loop
+        /// </summary>
+        public void WorkerLoop()
+        {
+            DateTime LastStart;
+            double NextProc = 0;
+
+            while (!Closing)
+            {
+                LastStart = DateTime.Now;
+
+                // Here we call the Work to do
+                this.DoWork();
+
+                NextProc = Interval - (DateTime.Now - LastStart).TotalMilliseconds;
+                if (NextProc > 0.0d)
+                {
+                    Thread.Sleep((int)(NextProc));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Method that gets called in a cycle
+        /// </summary>
+        public void DoWork()
+        {
+            //Work here
+
+            // or in Event....
+            if (this.Work != null)
+                this.Work(this, new EventArgs());
+        }
+
+        /// <summary>
+        /// Shutodwn thread. Needs to be called when exiting the program
+        /// </summary>
+        public void Shutdown()
+        {
+            if (WorkerThread == null)
+                return;
+
+            this.Closing = true;
+
+            WorkerThread.Join();
+        }
+
+    }
+}
---
+++ tvnCommand.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Threading;
+
+namespace TVN_APP
+{
+    class tvnCommand
+    {
+        public static void tvnCE(string ip, string password)
+        {
+            string rsnNodeInfo = $"tvnviewer {ip} -password={password}";
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.Verb = "runas";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine(@"cd c:\program files\tightvnc");
+            cmd.StandardInput.WriteLine(@rsnNodeInfo);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            //cmd.WaitForExit();
+            //Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+        }
+
+        public static void tvnCK()
+        {
+            string rsnNodeInfo = "taskkill /IM tvnviewer.exe /F";
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.Verb = "runas";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            cmd.StandardInput.WriteLine(@"cd c:\program files\tightvnc");
+            cmd.StandardInput.WriteLine(@rsnNodeInfo);
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            //cmd.WaitForExit();
+            //Console.WriteLine(cmd.StandardOutput.ReadToEnd());
+        }
+
+
+    }
+    
+}
